d826311c2001fc4408ba3e4c3974375edd5355a9
felipedosreismonteiro <felipe.dreis.monteiro@gmail.com>
2022-08-31 22:26:21 -0300

    Todos os quizes na pagina inicial com versao mobile

---
+++ assets/javaScript.js
@@ -0,0 +1,24 @@
+const quizes = document.querySelector(".Quizes");
+
+function allQuizes() { //buscando todos os quizes
+    const promise = axios.get('https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes');
+    promise.then(chegou)
+}
+allQuizes()
+
+function chegou(res) {
+    quizes.innerHTML = ""
+    res.data.forEach(element => {
+        quizes.innerHTML += 
+        `
+       <div class="imgQuizes">
+            <a href="#">
+                <div class="img">
+                    <h1>${element.title}</h1>
+                    <img src="${element.image}">
+                </div>
+            </a>
+        </div>
+        `
+    });
+}
